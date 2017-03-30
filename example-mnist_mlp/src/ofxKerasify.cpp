81cb76f91b8564ac95cf2bc14b8a2287c57f9802
Nao Tokui <tokui@qosmo.jp>
2017-03-30 18:50:01 +0900

    added: submodule kerasify

---
+++ example-mnist_mlp/src/ofxKerasify.cpp
@@ -0,0 +1,9 @@
+//
+//  ofxKerasify.cpp
+//  example-mnist_mlp
+//
+//  Created by Nao Tokui on 3/30/17.
+//
+//
+
+#include "ofxKerasify.hpp"
---
+++ example-mnist_mlp/src/ofxKerasify.h
@@ -0,0 +1,14 @@
+//
+//  ofxKerasify.hpp
+//  example-mnist_mlp
+//
+//  Created by Nao Tokui on 3/30/17.
+//
+//
+
+#ifndef ofxKerasify_hpp
+#define ofxKerasify_hpp
+
+#include <stdio.h>
+
+#endif /* ofxKerasify_hpp */
